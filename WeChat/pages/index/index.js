eee4453417b5a9119a442f8f335b98960f49ee1e
ZHJ0125 <shandonghoujin@163.com>
2019-11-16 11:39:13 +0800

    Get access_token successful

--- WeChat/pages/index/index.js
+++ WeChat/pages/index/index.js
@@ -1,12 +1,23 @@
 //index.js
-//获取应用实例
-const app = getApp()
+const grant_type = 'client_credentials'
+const client_id = 'na7ZiKRvj3n1PvHdeOFSrbEh'
+const client_secret = 'bMNEYK6717ebFl2hYzpuL9n0qx8waEl0'
 
 Page({
-  data: {
-  },
-  onLoad: function () {
-  },
-  getUserInfo: function(e) {
+  get_access_token: function (res) {
+    wx.request({
+      url: 'https://aip.baidubce.com/oauth/2.0/token?grant_type=' + grant_type + '&client_id=' + client_id + '&client_secret=' + client_secret,
+      method: 'POST',
+      complete:function(res) {
+        console.log('Request complete')
+      },
+      success: function(res) {
+        console.log(res)
+      },
+      fail: function(res) {
+        console.log('Fail to request !')
+        console.log(res)
+      }
+    })
   }
 })
